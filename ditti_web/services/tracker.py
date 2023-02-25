e4be232b45a91011d3a6bfd6cf5758cdd283d3ed
Alex Paden <padenfool@gmail.com>
2023-02-25 16:52:25 -0600

    return all objects on get tracker entries

--- ditti_web/services/tracker.py
+++ ditti_web/services/tracker.py
@@ -3,19 +3,20 @@ from ditti_web.database import warpcast, supabase
 import logging
 import time
 
-
 class TrackerService:
     
     def get_follow_tracker_entries_by_fid(self, fid: int):
         try:
             res = supabase.from_('follow_trackers').select('*').eq('fid', fid).execute()
-            data = res.data[0]
-            follower_changes = {
-                "added": data["follower_changes"]["added"],
-                "removed": data["follower_changes"]["removed"]
-            }
-            data["follower_changes"] = follower_changes
-            return data
+            entries = []
+            for data in res.data:
+                follower_changes = {
+                    "added": data["follower_changes"]["added"],
+                    "removed": data["follower_changes"]["removed"]
+                }
+                data["follower_changes"] = follower_changes
+                entries.append(data)
+            return entries
         except Exception as e:
             logging.error(f"error in get_follow_tracker_entries_by_fid: {e}")
 
